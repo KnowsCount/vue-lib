1532700674cdafc0e524cacc7411e25126fe4210
KnowsCount <knowscount@gmail.com>
2021-04-20 11:07:44 +0800

    refactor: default export name

--- src/lib-components/index.js
+++ src/lib-components/index.js
@@ -1,8 +1,8 @@
 /*
  * @Date: 19/04/2021 20.31.32 +0800
  * @Author: KnowsCount
- * @LastEditTime: 20/04/2021 07.24.10 +0800
+ * @LastEditTime: 20/04/2021 11.06.37 +0800
  * @FilePath: /vue-lib/src/lib-components/index.js
  */
 /* eslint-disable import/prefer-default-export */
-export { default as VueLibSample } from './input-box.vue'
+export { default as InputBox } from './input-box.vue'
