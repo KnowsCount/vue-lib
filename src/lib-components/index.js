0fb0d1a4dfb386232c8ac174f5fec000b6a8df89
KnowsCount <knowscount@gmail.com>
2021-04-24 12:33:42 +0800

    fix: register components (resolve #19)

--- src/lib-components/index.js
+++ src/lib-components/index.js
@@ -1,9 +1,12 @@
 /*
  * @Date: 19/04/2021 20.31.32 +0800
  * @Author: KnowsCount
- * @LastEditTime: 23/04/2021 18.46.27 +0800
+ * @LastEditTime: 24/04/2021 12.20.28 +0800
  * @FilePath: /vue-lib/src/lib-components/index.js
  */
 /* eslint-disable import/prefer-default-export */
 export { default as InputBox } from './input-box.vue'
 export { default as SmashToSubmit } from './smash-to-submit.vue'
+export { default as Check } from './check.vue'
+export { default as FeedbackReactions } from './feedback-reactions.vue'
+export { default as FavouriteButton } from './favourite-button.vue'
