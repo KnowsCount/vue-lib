de9ea9ac49ee268a5a6733b7654b2e71345ca4fe
KnowsCount <knowscount@gmail.com>
2021-04-25 20:48:29 +0800

    feat(undone): initialise link-hover component (#10) 🤒

--- dev/serve.vue
+++ dev/serve.vue
@@ -1,7 +1,7 @@
 <!--
  * @Date: 19/04/2021 20.31.32 +0800
  * @Author: KnowsCount
- * @LastEditTime: 24/04/2021 21.46.57 +0800
+ * @LastEditTime: 25/04/2021 20.47.39 +0800
  * @FilePath: /vue-lib/dev/serve.vue
 -->
 <script>
@@ -14,6 +14,7 @@ import Check from '../src/lib-components/check.vue';
 import FeedbackReactions from '../src/lib-components/feedback-reactions.vue';
 import FavouriteButton from '../src/lib-components/favourite-button.vue';
 import PaperplaneButton from '../src/lib-components/paperplane-button.vue';
+import LinkHover from '../src/lib-components/link-hover.vue';
 
 export default Vue.extend({
 	name: 'ServeDev',
@@ -23,7 +24,8 @@ export default Vue.extend({
 		Check,
 		FeedbackReactions,
 		FavouriteButton,
-		PaperplaneButton
+		PaperplaneButton,
+		LinkHover
 	}
 });
 </script>
@@ -41,5 +43,7 @@ export default Vue.extend({
 		<favourite-button/>
 		<br />
 		<paperplane-button />
+		<br />
+		<link-hover />
 	</div>
 </template>
---
+++ src/lib-components/link-hover.vue
@@ -0,0 +1,81 @@
+<!--
+ * @Date: 25/04/2021 20.40.01 +0800
+ * @Author: KnowsCount
+ * @LastEditTime: 25/04/2021 20.47.16 +0800
+ * @FilePath: /vue-lib/src/lib-components/link-hover.vue
+-->
+
+<template>
+	<div>
+		<p>
+			Check out
+			<a href="#">
+				{{ text }}
+				<svg viewBox="0 0 70 36">
+					<path d="M6.9739 30.8153H63.0244C65.5269 30.8152 75.5358 -3.68471 35.4998 2.81531C-16.1598 11.2025 0.894099 33.9766 26.9922 34.3153C104.062 35.3153 54.5169 -6.68469 23.489 9.31527" />
+				</svg>
+			</a>
+			here
+		</p>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "link-hover",
+	props: {
+		text: {
+			type: String,
+			default: 'the link'
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+p {
+	--text: #2B3044;
+	--line: #BBC1E1;
+	--line-active: #275EFE;
+	font-size: 18px;
+	margin: 0;
+	color: var(--text);
+	a {
+		display: inline-block;
+		position: relative;
+		text-decoration: none;
+		color: inherit;
+		svg {
+			width: 76px;
+			height: 40px;
+			position: absolute;
+			left: 50%;
+			bottom: 0;
+			transform: translate(-50%, 7px) translateZ(0);
+			fill: none;
+			stroke: var(--stroke, var(--line));
+			stroke-linecap: round;
+			stroke-width: 2px;
+			stroke-dasharray: var(--offset, 69px) 278px;
+			stroke-dashoffset: 361px;
+			transition: stroke .25s ease var(--stroke-delay, 0s), stroke-dasharray .35s;
+		}
+		&:hover {
+			--stroke: var(--line-active);
+			--stroke-delay: .1s;
+			--offset: 180px;
+		}
+	}
+}
+
+* {
+	box-sizing: inherit;
+	&:before,
+	&:after {
+		box-sizing: inherit;
+	}
+	box-sizing: border-box;
+	-webkit-font-smoothing: antialiased;
+	font-family: 'Inter', Arial;
+}
+</style>
